c9665bb7701250658f72fc0c5a24fc73befca309
Andrii Vasyliev <sol@hiqdev.com>
2016-05-10 08:34:12 -0400

    csfixed

--- src/config/web.php
+++ src/config/web.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Asset Packagist
+ *
+ * @link      https://github.com/hiqdev/asset-packagist
+ * @package   asset-packagist
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
+
 $config = require HISITE_VENDOR_DIR . '/hiqdev/hisite-config.php';
 
 if (YII_DEBUG) {
