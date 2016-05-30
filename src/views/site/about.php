150f226ea539ccaed87272f5d7199d32953fe06f
Andrii Vasyliev <sol@hiqdev.com>
2016-05-30 06:45:27 +0000

    minor

--- src/views/site/about.php
+++ src/views/site/about.php
@@ -11,9 +11,9 @@ $this->title = 'About';
 
     <p>Here is going to be more information on how it works.</p>
 
-    <p>Basically it provides Bower and NPM packages information in the same way as packagist does it for Composer projects</p>
+    <p>Basically it provides information on Bower and NPM packages in the same way as Packagist does it for Composer packages.</p>
 
     <h1>Acknowledgements</h1>
 
-    <p>This project uses Francois Pluchino's <a href="https://github.com/francoispluchino/composer-asset-plugin">composer-asset-plugin</a> to convert Bower and NPM packages to composer format.</p>
+    <p>This project uses Francois Pluchino's <a href="https://github.com/francoispluchino/composer-asset-plugin">composer-asset-plugin</a> to convert Bower and NPM packages to Composer format.</p>
 </div>
