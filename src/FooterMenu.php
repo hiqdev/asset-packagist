b5b2fa709b2a67decb8af29d9393de340ce7fbb3
Andrii Vasyliev <sol@hiqdev.com>
2016-08-22 10:53:37 +0000

    + footer menu

---
+++ src/FooterMenu.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Hisite core package
+ *
+ * @link      https://github.com/hiqdev/hisite-core
+ * @package   hisite-core
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\assetpackagist;
+
+use Yii;
+
+class FooterMenu extends MainMenu
+{
+    protected $_addTo = 'footer';
+}
