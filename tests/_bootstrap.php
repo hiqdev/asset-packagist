4c94e8ff33ff7cc3c41da3f89c1ea568fa29bbe6
Andrii Vasyliev <sol@hiqdev.com>
2017-05-08 09:28:18 +0000

    fixed tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -8,6 +8,7 @@
  * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
  */
 
-error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL);
 
-require_once __DIR__ . '/../src/config/bootstrap.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
