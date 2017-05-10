257b2f5c66b19be0df607aebbc577c595bd0c9c8
Andrii Vasyliev <sol@hiqdev.com>
2017-05-10 12:15:25 +0000

    csfixed

--- src/log/YiiLogIO.php
+++ src/log/YiiLogIO.php
@@ -2,7 +2,7 @@
 /**
  * Asset Packagist.
  *
- * @see      https://github.com/hiqdev/asset-packagist
+ * @link      https://github.com/hiqdev/asset-packagist
  * @package   asset-packagist
  * @license   BSD-3-Clause
  * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
@@ -12,40 +12,46 @@ namespace hiqdev\assetpackagist\log;
 
 use Composer\IO\NullIO;
 use Yii;
-use const YII_DEBUG;
 
-class YiiLogIO extends NullIO {
-    
-    public function isVerbose() {
+class YiiLogIO extends NullIO
+{
+    public function isVerbose()
+    {
         return YII_ENV_TEST;
     }
-    
-    public function isVeryVerbose() {
+
+    public function isVeryVerbose()
+    {
         return YII_ENV_DEV;
     }
-    
-    public function isDebug() {
+
+    public function isDebug()
+    {
         return YII_DEBUG;
     }
-    
-    public function write($messages, $newline = true, $verbosity = self::NORMAL) {
-        Yii::trace($messages, __METHOD__);
-    }
-    
-    public function writeError($messages, $newline = true, $verbosity = self::NORMAL) {
+
+    public function write($messages, $newline = true, $verbosity = self::NORMAL)
+    {
         Yii::trace($messages, __METHOD__);
     }
 
-    public function overwrite($messages, $newline = true, $size = 80, $verbosity = self::NORMAL) {
-        Yii::trace($messages, __METHOD__);
-    }
-    
-    public function overwriteError($messages, $newline = true, $size = 80, $verbosity = self::NORMAL) {
+    public function writeError($messages, $newline = true, $verbosity = self::NORMAL)
+    {
         Yii::trace($messages, __METHOD__);
     }
 
-    public function log($level, $message, array $context = array()) {
+    public function overwrite($messages, $newline = true, $size = 80, $verbosity = self::NORMAL)
+    {
+        Yii::trace($messages, __METHOD__);
+    }
+
+    public function overwriteError($messages, $newline = true, $size = 80, $verbosity = self::NORMAL)
+    {
+        Yii::trace($messages, __METHOD__);
+    }
+
+    public function log($level, $message, array $context = [])
+    {
         Yii::trace($message, __METHOD__);
     }
-
 }
