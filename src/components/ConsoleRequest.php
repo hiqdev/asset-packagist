d96203e23addd8a3b38ce6d5c64bb13ee80f11a7
Andrii Vasyliev <sol@hiqdev.com>
2026-08-20 14:55:03 +0200

    add GuestUser and ConsoleRequest for sentry

---
+++ src/components/ConsoleRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace hiqdev\assetpackagist\components;
+
+class ConsoleRequest extends \yii\console\Request
+{
+    public function getUserIp(): ?string
+    {
+        return null;
+    }
+}
